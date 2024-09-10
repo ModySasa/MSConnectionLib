c5b30932dfe083fac0eb5989fe4b5cbac9f8246b
Mohamed <mohamed.safwat@systemira.net>
2024-09-11 01:16:07 +0300

    Post call with new urls

--- Sources/MSConnectionLib/Examples/SimpleResponseExample/PostNetworkManager.swift
+++ Sources/MSConnectionLib/Examples/SimpleResponseExample/PostNetworkManager.swift
@@ -8,11 +8,35 @@
 
 import Foundation
 
-actor PostNetworkManager {
+actor PostNetworkManager : PostNetworkManagerProtocol , BaseUrlProviding {
     private let networkManager = NetworkManager()
 
     func getPostData() async -> Result<[PostModel], MultipleDecodingErrors> {
-        let url = "https://jsonplaceholder.typicode.com/posts"
+        let url = apiUrl + getPostUrl
         return await networkManager.get(from: url, body: Optional<String>.none, responseType: [PostModel].self)
+    }
+}
+
+protocol PostNetworkManagerProtocol {
+    var getPostUrl: String { get }
+    func getPostData() async -> Result<[PostModel], MultipleDecodingErrors>
+}
+
+extension PostNetworkManagerProtocol {
+    var getPostUrl: String { APIs.PostUrls.main.url() }
+}
+
+fileprivate extension APIs {
+    enum PostUrls: String {
+        case main = "/posts"
+        case single = "/"
+        
+        func url()-> String {
+            if(self == .main) {
+                return self.rawValue
+            } else {
+                return APIs.PostUrls.main.rawValue + self.rawValue
+            }
+        }
     }
 }
