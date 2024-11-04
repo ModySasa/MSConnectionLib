6c14bc3b0ddcc58bface35ef2f87723004c4365f
Mohamed <mohamed.safwat@systemira.net>
2024-11-04 05:24:23 +0200

    Async funcs 2.0

--- Sources/MSConnectionLib/ResponseHandling/CommonResponse.swift
+++ Sources/MSConnectionLib/ResponseHandling/CommonResponse.swift
@@ -56,6 +56,82 @@ public struct CommonResponse<T: Codable>: Codable {
     }
 
     public func handleStatus(
+        onSuccess: @escaping () -> Void,
+        onFailure: ((String) -> Void)? = nil,
+        onStringStatus: ((String?) -> Void)? = nil,
+        handleErrors: ((ErrorResponse? , [String: [String]]??) -> Void)? = nil
+    ) {
+        switch self.status {
+        case .boolean(let value):
+            value ? onSuccess() : handleFailure(message: self.message, onFailure: onFailure, handleErrors: handleErrors)
+        case .string(let value):
+            print("String status: \(value)")
+            if let onStringStatus {
+                onStringStatus(value)
+            }
+            if let handleErrors {
+                handleErrors(error , errors)
+            }
+        case .int(let value):
+            print("Integer status: \(value)")
+            if value == 1 {
+                onSuccess()
+            } else {
+                if let message = self.message , let onFailure {
+                    onFailure(message)
+                }
+                if let handleErrors {
+                    handleErrors(error , errors)
+                }
+            }
+        case .yesNo(let value):
+            print("Yes/No status: \(value)")
+            if value {
+                onSuccess()
+            } else {
+                if let message = self.message , let onFailure {
+                    onFailure(message)
+                }
+                if let handleErrors {
+                    handleErrors(error , errors)
+                }
+            }
+        case .oneZero(let value):
+            if value {
+                onSuccess()
+            } else {
+                if let message = self.message , let onFailure {
+                    onFailure(message)
+                }
+                if let handleErrors {
+                    handleErrors(error , errors)
+                }
+            }
+        case .unknown:
+            print("Unknown status")
+            if let onStringStatus {
+                onStringStatus(self.message)
+            }
+            if let handleErrors {
+                handleErrors(error , errors)
+            }
+        }
+    }
+    
+    private func handleFailure(
+         message: String?,
+         onFailure: ((String) -> Void)?,
+         handleErrors: ((ErrorResponse?, [String: [String]]??) -> Void)?
+     ) {
+         if let message = message, let onFailure {
+             onFailure(message)
+         }
+         if let handleErrors {
+             handleErrors(error, errors)
+         }
+     }
+    
+    public func handleStatus(
         onSuccess: @escaping () async -> Void,
         onFailure: ((String) async -> Void)? = nil,
         onStringStatus: ((String?) async -> Void)? = nil,
