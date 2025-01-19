5eba7c8c91139cf7b0bb371595fd5b44acb6a773
Mo.Aziz <mohamed.aziz@systemira.net>
2025-01-19 13:38:29 +0200

    d

--- Sources/MSConnectionLib/Managers/NetworkManager.swift
+++ Sources/MSConnectionLib/Managers/NetworkManager.swift
@@ -32,11 +32,11 @@ public actor NetworkManager {
         token: String? = nil
         , shouldDumpRequest : Bool = false
     ) async -> Result<T, MultipleDecodingErrors> {
-        print("```````` url \(url)")
+
         guard var components = URLComponents(string: url) else {
             return .failure(MultipleDecodingErrors(errors: [.other(URLError(.badURL))]))
         }
-        print("```````` components \(components.url)")
+
         if let body = parameters {
             let jsonData = try? JSONEncoder().encode(body)
             if let jsonData = jsonData,
@@ -51,9 +51,7 @@ public actor NetworkManager {
                 components.queryItems = existingQueryItems
             }
         }
-        
-        print("```````` components after \(components.url)")
-        
+                
         guard let theUrl = components.url else {
             return .failure(MultipleDecodingErrors(errors: [.other(URLError(.badURL))]))
         }
