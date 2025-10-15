03c0636b51f2aae4f8256b9af6b9cc09f6ad8d69
Mo.Aziz <mohamed.aziz@systemira.net>
2025-10-15 11:46:35 +0300

    network monitor

---
+++ Sources/MSConnectionLib/Helpers/NetworkMonitor.swift
@@ -0,0 +1,26 @@
+import Network
+import SwiftUI
+
+public class NetworkMonitor: ObservableObject {
+    private let monitor = NWPathMonitor()
+    private let queue = DispatchQueue(label: "NetworkMonitor")
+    
+    @Published public var isConnected = true
+    @Published public var connectionType: NWInterface.InterfaceType?
+    
+    init() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            DispatchQueue.main.async {
+                self?.isConnected = path.status == .satisfied
+                self?.connectionType = path.usesInterfaceType(.wifi) ? .wifi :
+                                       path.usesInterfaceType(.cellular) ? .cellular :
+                                       path.usesInterfaceType(.wiredEthernet) ? .wiredEthernet : nil
+            }
+        }
+        monitor.start(queue: queue)
+    }
+    
+    deinit {
+        monitor.cancel()
+    }
+}
